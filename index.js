1a927a5392c08bc893f9e44dc23b0136ddbd61d7
PrafulC7 <prafullacharde00715@gmail.com>
2025-12-22 08:34:06 +0530

    cors *

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const app = express()
 app.use(express.json())
 const cors = require("cors");
 const corsOptions = {
-  origin: [
-    "https://book-shop-app-sable.vercel.app",
-    "http://localhost:5173",
-    "http://localhost:3000"
-  ],
-  credentials: true,
-  optionSuccessStatus: 200,
+  origin: "*",
 };
 
 app.use(cors(corsOptions));
